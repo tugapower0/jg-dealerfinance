0c3b7f0f09b11e559e0b159b28eb73cc83e1f263
Lindholm <Christoffer.brahe@gmail.com>
2024-06-11 22:06:59 +0200

    Template Upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "cerulean"
+game "gta5"
+
+title "LB Phone - App Template | React TS"
+description "A template for creating apps for the LB Phone."
+author "Breze & Loaf"
+
+client_script "client.lua"
+
+files {
+    "ui/dist/**/*",
+    "ui/icon.png"
+}
+
+ui_page "ui/dist/index.html"
+-- ui_page "http://localhost:3000"
